e2801c81923f960e0fb8b5569ffd18995f209af5
hillary <ngangahillary84@gmail.com>
2025-10-30 17:39:40 +0300

    Added deployment files

--- cyber/cyber/settings.py
+++ cyber/cyber/settings.py
@@ -132,6 +132,7 @@ TIME_ZONE = 'Africa/Nairobi'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "cyberapp"/"static",
